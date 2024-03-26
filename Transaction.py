225ac233f8d924dfa7f68caa211fd59379a333ff
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-03-26 15:12:44 +0100

    Transaction with comments

--- Transaction.py
+++ Transaction.py
@@ -1,24 +1,28 @@
 import datetime
 
 class Transaction:
-    def __init__(self, name, description, amount, type, date):
+    def __init__(self, name, description, amount, category, type, date):
         self.name = name
         self.description = description
         self.amount = amount
-        self.type = type
+        self.category = category
+        self.type = type                        # Income or Expense
         self.date = datetime.datetime.strptime(date, "%Y-%m-%d")
 
-    def getName(self):
+    def getName(self):                          # Get the name of the transaction
         return self.name
     
-    def getDescription(self):  
+    def getDescription(self):                   # Get the description of the transaction
         return self.description
     
-    def getAmount(self):    
+    def getAmount(self):                        # Get the amount of the transaction
         return self.amount
     
-    def getType(self):    
+    def getCategory(self):                      # Get the category of the transaction
+        return self.category
+    
+    def getType(self):                          # Get the type of the transaction (Income or Expense)
         return self.type
     
-    def getDate(self):    
+    def getDate(self):                          # Get the date of the transaction
         return self.date
