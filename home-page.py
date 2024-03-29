836dac3d6b4350e61bdf19755b5e80ffab8df768
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-03-29 09:48:42 +0100

    GUI updated, Home Page with input working

--- home-page.py
+++ home-page.py
@@ -6,18 +6,34 @@ from GUI import GUI
 def homePage():
     
     gui = GUI()
-    window = gui.createWindow()
+    window = gui.createWindow("Home Page")
     clock = pygame.time.Clock()
     uiRefreshRate = clock.tick(60) / 10000.0
     gui.createImage(window, 200, 175, 300, 300, "images/logo.png")
-    gui.createButton(window, 100, 500, 200, 30, "LOGIN")
-    gui.createButton(window, 100, 550, 200, 30, "CREATE NEW ACCOUNT")
+
+    loginButton = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((100, 500), (200, 30)),
+        text="LOGIN",
+        manager=gui.MANAGER,
+    )
+    createAccountButton = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((100, 550), (200, 30)),
+        text="CREATE NEW ACCOUNT",
+        manager=gui.MANAGER,
+    )
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == loginButton:
+                        print("Login button pressed")
+                    elif event.ui_element == createAccountButton:
+                        print("Create Account button pressed")
+
             gui.MANAGER.process_events(event)
         gui.MANAGER.update(uiRefreshRate)
         gui.MANAGER.draw_ui(window)
