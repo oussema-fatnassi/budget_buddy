f2c34e728b4c0aff3053903b446b5e317469fcfe
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-03-29 10:16:24 +0100

    GUI updated

--- GUI.py
+++ GUI.py
@@ -17,9 +17,9 @@ class GUI:
         self.dropDown = None
         self.passwordInput = None
 
-    def createWindow(self):                                                                         # Method to create the window
+    def createWindow(self, windowTitle):                                                                         # Method to create the window
         pygame.init()
-        pygame.display.set_caption("Window")
+        pygame.display.set_caption(windowTitle)
         window = pygame.display.set_mode((self.windowWidth, self.windowHeight))
         window.fill(self.BACKGROUND)
         self.MANAGER = pygame_gui.UIManager((self.windowWidth, self.windowHeight))
@@ -44,7 +44,6 @@ class GUI:
             relative_rect=pygame.Rect((pos_x, pos_y), (width, height)),
             text=text,
             manager=self.MANAGER,
-            object_id="button"
         )
 
     def createLabel(self, window, pos_x, pos_y, width, height, text):                               # Method to create the label
@@ -55,24 +54,27 @@ class GUI:
             object_id="label"
         )
 
-    def createDropDown(self, window, pos_x, pos_y, width, height, options):                         # Method to create the drop down menu
+    def createDropDown(self, window, pos_x, pos_y, width, height, options, id):                         # Method to create the drop down menu
         self.dropDown = pygame_gui.elements.UIDropDownMenu(
             relative_rect=pygame.Rect((pos_x, pos_y), (width, height)),
             options_list=options,
             starting_option=options[0],
             manager=self.MANAGER,
-            object_id="drop_down"
+            object_id=id
         )
         self.dropDown.expand_on_option_click=False
     
-    def createSelectionList(self, window, pos_x, pos_y, width, height, options, bool):                     # Method to create the selection list
-        self.selectionList = pygame_gui.elements.UISelectionList(
-            relative_rect=pygame.Rect((pos_x, pos_y), (width, height)),
-            item_list=options,
-            allow_double_clicks=True,
-            manager=self.MANAGER,
-            object_id="selection_list"
-        )
+    def createSelectionList(self, window, pos_x, pos_y, width, height, options, bool):
+            self.selectionList = pygame_gui.elements.UISelectionList(
+                relative_rect=pygame.Rect((pos_x, pos_y), (width, height)),
+                item_list=options,
+                allow_double_clicks=True,
+                manager=self.MANAGER,
+                object_id="selection_list"
+            )
+            # Store a dictionary mapping each item to its corresponding message
+            self.item_messages = {item: f"Message for {item}" for item in options}
+
     
     def createPasswordInput(self, window, pos_x, pos_y, width, height):                             # Method to create the password input field
         self.passwordInput = pygame_gui.elements.UITextEntryLine(
@@ -84,19 +86,26 @@ class GUI:
     def set_text_hidden(self, is_hidden=True):                                                      # Method to hide the password input field
         self.passwordInput.set_text_hidden(is_hidden)
 
-    def createMessageBox(self, window, pos_x, pos_y, width, height, text):                          # Method to create the message box
-        self.messageBox = pygame_gui.elements.UIWindow(
+    def createMessageBox(self, window, pos_x, pos_y, width, height, text):
+        self.messageBox = pygame_gui.windows.UIMessageWindow(
             rect=pygame.Rect((pos_x, pos_y), (width, height)),
+            html_message=text,  
             manager=self.MANAGER,
-            window_display_title = 'Message Box',
+            window_title='Message Box',  
             object_id="message_box"
-            # draggable=False
         )
-        print("Message box created.")
 
+
+    def createTextBox(self, window, pos_x, pos_y, width, height, text):                             # Method to create the text box
+        self.textBox = pygame_gui.elements.UITextBox(
+            relative_rect=pygame.Rect((pos_x, pos_y), (width, height)),
+            html_text=text,
+            manager=self.MANAGER,
+            object_id="text_box"
+        )
 
     def main(self):                                                                                     # Main method to run the GUI
-        window = self.createWindow()
+        window = self.createWindow("GUI TEST")
         self.createTextInput(window, 50, 300, 300, 30)
         self.createButton(window, 50, 350, 100, 30, "LOGIN")
         self.createLabel(window, 50, 250, 100, 30, "Email")
@@ -105,6 +114,7 @@ class GUI:
         self.set_text_hidden(True)
         self.createImage(window, 200, 100, 100, 100, "images/Logo.png") 
         self.createSelectionList(window, 50, 450, 200, 70, ["Option 1", "Option 2", "Option 3"],True) 
+        self.createTextBox(window, 50, 180, 300, 50, "YOU ARE POOR! GO TO WORK!")
 
         while True:
             for event in pygame.event.get():
@@ -130,14 +140,15 @@ class GUI:
                             self.messageBox.kill()
                     elif event.user_type == pygame_gui.UI_SELECTION_LIST_DOUBLE_CLICKED_SELECTION:
                         if event.ui_element == self.selectionList:
-                            self.createMessageBox(window, 50, 50, 300, 300, "This is a message box.")
+                            selected_item = event.text
+                            message = self.item_messages[selected_item]
+                            self.createMessageBox(window, 50, 50, 300, 300, message)
 
             window.fill(self.BACKGROUND)  
             self.MANAGER.update(self.uiRefreshRate)
             self.createImage(window, 200, 100, 100, 100, "images/Logo.png")  
             self.MANAGER.draw_ui(window)  
             pygame.display.update()  
-
 
 if __name__ == "__main__":
     window = GUI()
