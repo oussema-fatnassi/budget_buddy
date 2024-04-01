d019dbf3d1fe63e1deaee91358325f01e19d8625
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-04-01 15:16:39 +0200

    Sort by category from db working

--- sort_by_category.py
+++ sort_by_category.py
@@ -5,6 +5,7 @@ from pygame_gui.windows.ui_message_window import UIMessageWindow
 from GUI import GUI
 from pygame_gui.core import ObjectID
 from page_manager import PageManager
+import database_operation
 
 def sortByCategory(retrieved_user):
     gui = GUI()
@@ -15,8 +16,8 @@ def sortByCategory(retrieved_user):
     label = gui.createLabel(window, 50, 100, 300, 50, "Select the category to filter by")
 
     lastTransactionsList = pygame_gui.elements.UISelectionList(
-        relative_rect=pygame.Rect((50, 200), (300, 300)),
-        item_list=["Transaction 1", "Transaction 2", "Transaction 3", "Transaction 4", "Transaction 5"],
+        relative_rect=pygame.Rect((50, 200), (300, 280)),
+        item_list=[],
         manager=gui.MANAGER,
         object_id=ObjectID("selection_list")
     )
@@ -40,6 +41,10 @@ def sortByCategory(retrieved_user):
     object_id="close_button"
     )
 
+    all_transactions = database_operation.get_all_transactions(retrieved_user[0])
+    lastTransactionsList.add_items(all_transactions)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -52,17 +57,30 @@ def sortByCategory(retrieved_user):
                 if event.user_type == pygame_gui.UI_SELECTION_LIST_DOUBLE_CLICKED_SELECTION:
                     if event.ui_element == lastTransactionsList:
                         selected_item = event.text 
-                        UIMessageWindow(
+                        transaction_details = database_operation.get_transaction_details(selected_item, retrieved_user[0])
+                        details_text = ""
+                        if transaction_details:
+                            details_text = f"<b>Name:</b> {transaction_details['name']}<br>" \
+                               f"<b>Description:</b> {transaction_details['description']}<br>" \
+                               f"<b>Amount: € </b> {transaction_details['amount']}<br>" \
+                               f"<b>Category:</b> {transaction_details['category']}<br>" \
+                               f"<b>Type:</b> {transaction_details['type']}<br>" \
+                               f"<b>Date:</b> {transaction_details['date']}"
+                        pygame_gui.windows.UIMessageWindow(
                             rect=pygame.Rect((50, 50), (300, 300)),
-                            html_message=selected_item,  
+                            html_message= details_text,
                             manager=gui.MANAGER,
-                            window_title='Message Box',
+                            window_title='Transaction Details',
                             object_id="message_box"
                         )
 
                 elif event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                     if event.ui_element == confirmButton:
-                        pass
+                        selected_category = categoryList.selected_option.upper()
+                        transactions = database_operation.get_transactions_by_category(retrieved_user[0], selected_category)
+                        lastTransactionsList.remove_items(all_transactions)
+                        lastTransactionsList.add_items(transactions)
+                        
                     if event.ui_element == closeButton:
                         PageManager.show_filter_page(retrieved_user)
 
